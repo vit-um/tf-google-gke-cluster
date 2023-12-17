94b63064de18a7c798b349d0cbfda70831fac129
VITALII UMANETS <umanetsvitaliy@gmail.com>
2023-12-17 17:48:06 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "GOOGLE_PROJECT" {
 
 variable "GOOGLE_REGION" {
   type        = string
-  default     = "us-central1-c"
+  default     = "europe-west3-c"
   description = "GCP region to use"
 }
 
